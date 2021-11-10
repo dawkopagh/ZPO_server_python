bad48bf65ce23bf1a4bc21737558151dc66ba5e1
Dawid Kopeć <92300144+dawkopagh@users.noreply.github.com>
2021-11-10 08:57:51 +0100

    final servers + test

--- servers.py
+++ servers.py
@@ -1,69 +1,92 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, TypeVar
 from abc import ABC, abstractmethod
+import re
 
 
 class Product:
-    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
-    def __init__(self,name: str, price: float):
-        self.name = name
-        self.price = price
 
-    def __eq__(self, other):
-        return self.name == other.name and self.price == other.price  # FIXME: zwróć odpowiednią wartość
+    def __init__(self, name: str, price: float) -> None:
+
+        match_pattern = "^[a-zA-Z]+[0-9]+$"
+        if not re.fullmatch(match_pattern, name):
+            raise ValueError
+
+        self.name: str = name
+        self.price: float = price
+
+    def __eq__(self, other) -> bool:
+        return self.name == other.name and self.price == other.price
 
     def __hash__(self):
         return hash((self.name, self.price))
 
-    # TODO: Dodać wyjątek ValueError
+
+class ServerError(Exception):
+    def __init__(self, server, msg=None):
+        if msg is None:
+            msg = f"An error occured with server {server}"
+        super().__init__(msg)
+        self.server = server
 
 
-class TooManyProductsFoundError:
+class TooManyProductsFoundError(ServerError, ValueError):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
-    pass
+    def __init__(self, msg):
+        super().__init__(msg)
 
 
 class Server(ABC):
-    n_max_returned_intries= int
+    n_max_returned_entries: int = 3
 
     def __init__(self, *args, **kwargs):
-        super.__init__(*args,**kwargs)
+        super().__init__(*args, **kwargs)
+
+    def get_entries(self, n_letters: Optional[int] = 1) -> List[Product]:
+        match_pattern = "^[a-zA-Z]{{{n_letters}}}[0-9]{{2,3}}$".format(n_letters=n_letters)
+        entries_list = [product for product in self._get_products_list() if re.match(match_pattern, product.name)]
+        if len(entries_list) > Server.n_max_returned_entries:
+            raise TooManyProductsFoundError("Too many products on server.")
+        return sorted(entries_list, key=lambda product: product.price)
 
     @abstractmethod
-    def get_entries(self, n_letters: Optional[int]=1): List[Product]
+    def _get_products_list(self, n_letters: Optional[int] = 1) -> List[Product]:
+        raise NotImplementedError
 
-# FIXME: Każada z poniższych klas serwerów powinna posiadać:
-#   (1) metodę inicjalizacyjną przyjmującą listę obiektów typu `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze,
-#   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
-#   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
+
+HelperType = TypeVar('HelperType', bound=Server)
+
 
 class ListServer(Server):
     def __init__(self, products: List[Product], *args, **kwargs):
         self.products = products
-        super.__init__(*args, *kwargs)
+        super().__init__(*args, *kwargs)
 
-    def get_entries(self, n_letters: Optional[int]=1) -> List[Product]:
-
-
-
+    def _get_products_list(self, n_letters: Optional[int] = 1) -> List[Product]:
+        return self.products
 
 
 class MapServer(Server):
     def __init__(self, products: List[Product], *args, **kwargs):
-        self.products = {elem.name : elem for elem in products}
-        super.__init__(*args, *kwargs)
+        self.products: Dict[str, Product] = {elem.name: elem for elem in products}
+        super().__init__(*args, *kwargs)
 
-    def get_entries(self, n_letters: Optional[int] = 1) -> List[Product]:
-        matching_products: List[Product] = []
-        for prod in self.products:
-            if prod.name
+    def _get_products_list(self, n_letters: Optional[int] = 1) -> List[Product]:
+        return list(self.products.values())
 
 
 class Client:
-    def __init__(self, server: Server):
+    def __init__(self, server: HelperType):
         self.server = server
 
-    def get_total_price(self, n_letters: Optional[int]): Optional[float]
-        raise NotImplementedError()
+    def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
+        try:
+            if n_letters is None:
+                matching_products = self.server.get_entries()
+            else:
+                matching_products = self.server.get_entries(n_letters)
+            return sum([product.price for product in matching_products])
+        except TooManyProductsFoundError:
+            return None
---
+++ servers__test.py
@@ -0,0 +1,52 @@
+import unittest
+from servers import Client, Product, ListServer, MapServer, TooManyProductsFoundError
+from collections import Counter
+
+server_types = MapServer, ListServer
+
+
+class ClientTest(unittest.TestCase):
+    def test_price_equal_zero_if_exception(self):
+        products = [Product('ABC2137', 2)] * 4
+        server = ListServer(products)
+        client = Client(server)
+
+        self.assertEqual(0, client.get_total_price(2))
+
+    def test_total_price_for_normal_execution(self):
+        products = [Product('PP234', 2), Product('PR235', 3), Product('PP235', 3), Product('PN235', 3)]
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            self.assertEqual(None, client.get_total_price(2))
+
+
+class ServerTest(unittest.TestCase):
+    def test_get_entries_returns_proper_entries(self):
+        products = [Product('PEE12', 1), Product('R4567', 2), Product('R235', 1), Product('PPG1', 1),
+                    Product('PPR23', 4)]
+        for server_type in server_types:
+            server = server_type(products)
+            entries = server.get_entries(3)
+            self.assertEqual(Counter([products[0], products[4]]), Counter(entries))
+
+
+class ExceptTest(unittest.TestCase):
+    def test_too_many_products_exception(self):
+        with self.assertRaises(TooManyProductsFoundError):
+            products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1), Product('PP236', 3),
+                        Product('PP237', 2)]
+            for server_type in server_types:
+                server = server_type(products)
+                return server.get_entries(2)
+
+    def test_incorrect_products_name(self):
+        with self.assertRaises(ValueError):
+            products = [Product('2137', 1), Product('PP234', 2), Product('PP235', 1), Product('PP236', 3),
+                        Product('PP237', 2)]
+            for server_type in server_types:
+                server = server_type(products)
+                return server.get_entries(2)
+
+if __name__ == '__main__':
+    unittest.main()
